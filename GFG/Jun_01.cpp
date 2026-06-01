816ed307e18a9d505725ee0087468c1ecd14d50c
Darshika Bhasker <darshikabhasker@gmail.com>
2026-06-01 15:40:03 +0530

    jun 1 gfg and leet done

---
+++ GFG/Jun_01.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector> 
+
+using namespace std;
+
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+       int largest= arr[0];
+       for(int i = 1 ; i<arr.size() ; i++)
+       {
+           if(arr[i]>largest )
+           {
+               largest = arr[i];
+           }
+       }
+       return largest;
+    }
+};
+int main() {
+    
+    Solution obj;
+    
+    vector <int> arr={101,2,343,56,564};
+    int result = obj.largest(arr);
+    
+    cout << "Largest number in array: " << result << endl;
+    
+    return 0;
+}
---
+++ LeetCode/Jun01.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        for (int i = num.length() - 1; i >= 0; i--) 
+        {
+            if ((num[i] - '0') % 2 != 0) 
+            {
+                return num.substr(0, i+1);
+            } 
+        }
+        return "";
+    }
+};
+int main() {
+    
+    Solution obj;
+    
+    string num = "35427";
+    string result = obj.largestOddNumber(num);
+    
+    cout << "Largest odd number substring: " << result << endl;
+    
+    return 0;
+}
